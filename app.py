e43ab6a476f0a4fac2023062e21fad93b94a4158
Savita Karthikeyan <savitakartik@gmail.com>
2023-08-10 14:47:44 +0100

    initial implementation of trees tab
    implemented  PR review suggestions

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ import holoviews.operation.datashader as hd
 import tskit
 import utils
 import bokeh.models as bkm
+import pathlib
+import functools
 
 # Usage: panel serve app.py --args /path/to/trees-file
-path = sys.argv[1]
+path = pathlib.Path(sys.argv[1])
+trees_file = path.name
 ts = tskit.load(path)
 ti = utils.TreeInfo(ts, 1)
 
-
 df_mutations = ti.mutations_data()
 df_edges = ti.edges_data()
-
+df_trees = ti.trees_data()
 
 # Global plot settings
 plot_width = 1000
@@ -72,6 +74,22 @@ def make_hist(data, title, bins_range, log_y=True, plot_width=800):
     return histogram
 
 
+def make_hist_matplotlib(data, title, num_bins, log_y=True, xlim=(None, None)):
+    ### Make histogram from given count data using parameters suitable for the matplotlib backend
+    if xlim[1] is not None:
+        data = data[data < xlim[1]]
+    if xlim[0] is not None:
+        data = data[data > xlim[0]]
+    count, bins = np.histogram(data, bins=num_bins)
+    ylabel = "log(Count)" if log_y else "Count"
+    np.seterr(divide="ignore")
+    if log_y:
+        count = np.log10(count)
+        count[count == -np.inf] = 0
+
+    return hv.Histogram((count, bins)).opts(title=title, ylabel=ylabel)
+
+
 def make_hist_panel(log_y):
     ### Make row of histograms for holoviews panel
     overall_site_hist = make_hist(
@@ -97,6 +115,8 @@ def page1():
 
 
 def page2():
+    hv.extension("bokeh")
+    plot_width = 1000
     log_y_checkbox = pn.widgets.Checkbox(
         name="Log y-axis of Mutations per site/node plots", value=False
     )
@@ -136,6 +156,7 @@ def page2():
 
 
 def page3():
+    hv.extension("bokeh")
     df_edges["parent_time_right"] = df_edges["parent_time"]
     lines = hv.Segments(
         df_edges, kdims=["left", "parent_time", "right", "parent_time_right"]
@@ -167,6 +188,7 @@ def page3():
 
 
 def page4():
+    hv.extension("bokeh")
     node_id_input = pn.widgets.TextInput(value="", name="Node ID")
     df_edges["parent_time_right"] = df_edges["parent_time"]
     tabulator = pn.widgets.Tabulator(show_index=False)
@@ -207,10 +229,170 @@ def page4():
     return pn.Column(node_id_input, dynamic_plot, tabulator)
 
 
+def page5():
+    hv.extension("matplotlib")
+    max_num_sites = int(df_trees.num_sites.max())
+    min_num_sites = int(df_trees.num_sites.min())
+    sites_default_num_bins = min(int(np.sqrt(max_num_sites)), 50)
+
+    spans = df_trees.right - df_trees.left
+    min_span = int(np.min(spans))
+    max_span = int(np.max(spans))
+    spans_default_num_bins = min(50, int(np.sqrt(max_span)))
+
+    sites_hist_func = functools.partial(
+        make_hist_matplotlib,
+        df_trees.num_sites,
+        "Sites per tree",
+        num_bins=sites_default_num_bins,
+        log_y=True,
+        xlim=(min_num_sites, max_num_sites),
+    )
+
+    spans_hist_func = functools.partial(
+        make_hist_matplotlib,
+        spans,
+        "Genomic span per tree",
+        num_bins=spans_default_num_bins,
+        log_y=True,
+        xlim=(min_span, max_span),
+    )
+
+    if len(df_trees) > 1:
+        sites_log_checkbox = pn.widgets.Checkbox(
+            name="log y-axis", value=True, align=("center")
+        )
+
+        sites_xlim_slider = pn.widgets.IntRangeSlider(
+            name="x limits",
+            start=min_num_sites,
+            value=(min_num_sites, max_num_sites),
+            end=max_num_sites,
+            step=10,  # TODO set a sensible step size
+            align=("center"),
+        )
+
+        sites_bins_slider = pn.widgets.IntSlider(
+            name="Number of bins",
+            start=1,
+            end=max(500, int(max_num_sites)),  # TODO set a sensible end value
+            value=sites_default_num_bins,
+            step=1,  # TODO set a sensible step size
+            align=("center"),
+        )
+
+        sites_reset_checkbox = pn.widgets.Checkbox(
+            name="reset", value=True, align=("center")
+        )
+
+        def reset_sites(reset=False):
+            if reset:
+                sites_log_checkbox.value = True
+                sites_xlim_slider.value = (min_num_sites, max_num_sites)
+                sites_bins_slider.value = sites_default_num_bins
+                sites_reset_checkbox.value = False
+
+        sites_reset_panel = pn.bind(reset_sites, reset=sites_reset_checkbox)
+
+        sites_hist_panel = pn.bind(
+            sites_hist_func,
+            log_y=sites_log_checkbox,
+            xlim=sites_xlim_slider,
+            num_bins=sites_bins_slider,
+        )
+
+        spans_log_checkbox = pn.widgets.Checkbox(
+            name="log y-axis", value=True, align=("center")
+        )
+
+        spans_xlim_slider = pn.widgets.IntRangeSlider(
+            name="Maximum tree span",
+            start=min_span,
+            value=(min_span, max_span),
+            end=max_span,
+            step=1_000,  # TODO set a sensible step size
+            align=("center"),
+        )
+
+        spans_bins_slider = pn.widgets.IntSlider(
+            name="Number of bins",
+            start=1,
+            end=min(int(max_span), 500),  # TODO set a sensible end value
+            value=spans_default_num_bins,
+            step=1,  # TODO set a sensible step size
+            align=("center"),
+        )
+
+        spans_reset_checkbox = pn.widgets.Checkbox(
+            name="reset", value=True, align=("center")
+        )
+
+        def reset_spans(reset=False):
+            if reset:
+                spans_log_checkbox.value = True
+                spans_xlim_slider.value = (min_span, max_span)
+                spans_bins_slider.value = spans_default_num_bins
+                spans_reset_checkbox.value = False
+
+        spans_reset_panel = pn.bind(reset_spans, reset=spans_reset_checkbox)
+
+        spans_hist_panel = pn.bind(
+            spans_hist_func,
+            log_y=spans_log_checkbox,
+            xlim=spans_xlim_slider,
+            num_bins=spans_bins_slider,
+        )
+
+        hist_panel = pn.Row(
+            pn.Column(
+                sites_reset_panel,
+                sites_hist_panel,
+                pn.Column(
+                    pn.pane.Markdown("Plot Options:", align=("center")),
+                    sites_reset_checkbox,
+                    sites_log_checkbox,
+                    sites_xlim_slider,
+                    sites_bins_slider,
+                ),
+            ),
+            pn.Column(
+                spans_reset_panel,
+                spans_hist_panel,
+                pn.Column(
+                    pn.pane.Markdown("Plot Options:", align=("center")),
+                    spans_reset_checkbox,
+                    spans_log_checkbox,
+                    spans_xlim_slider,
+                    spans_bins_slider,
+                ),
+            ),
+        )
+    else:
+        sites_hist_single_tree = make_hist_matplotlib(
+            df_trees.num_sites, "Sites per tree", num_bins="auto", log_y=False
+        )
+        sites_hist_single_tree.opts(xticks=[df_trees.num_sites[0]], yticks=[1])
+        spans_hist_single_tree = make_hist_matplotlib(
+            spans, "Genomic span per tree", num_bins="auto", log_y=False
+        )
+        spans_hist_single_tree.opts(xticks=[spans[0]], yticks=[1])
+        hist_panel = pn.Row(
+            pn.Column(sites_hist_single_tree), pn.Column(spans_hist_single_tree)
+        )
+
+    return hist_panel
+
+
 pn.extension(sizing_mode="stretch_width")
 pn.extension("tabulator")
 
-pages = {"Overview": page1, "Mutations": page2, "Edges": page3, "Edge Explorer": page4}
+pages = {
+    "Overview": page1,
+    "Mutations": page2,
+    "Edges": page3,
+    "Edge Explorer": page4,
+    "Trees": page5,
+}
 
 
 def show(page):
@@ -224,18 +406,19 @@ page = pn.widgets.RadioButtonGroup(
     name="Page",
     # sizing_mode="fixed",
     button_type="success",
+    orientation="vertical",
 )
 ishow = pn.bind(show, page=page)
 pn.state.location.sync(page, {"value": "page"})
 
 ACCENT_COLOR = "#0072B5"
 DEFAULT_PARAMS = {
-    "site": "Panel Multi Page App",
+    "site": "QC dashboard",
     "accent_base_color": ACCENT_COLOR,
     "header_background": ACCENT_COLOR,
 }
 pn.template.FastListTemplate(
-    title="As Single Page App",
+    title=f"{trees_file}",
     sidebar=[page],
     main=[ishow],
     **DEFAULT_PARAMS,
